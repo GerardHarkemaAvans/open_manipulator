1a61befeba759301c999f6c0ef2adb9c9ffbaf0b
Gerard Harkema <ga.harkema@avans.nl>
2021-12-14 14:54:28 +0100

    20211214-1

--- install/install.bash
+++ install/install.bash
@@ -4,5 +4,8 @@ sudo apt update
 sudo apt-get install ros-melodic-dynamixel-sdk ros-melodic-dynamixel-workbench*
 sudo apt-get install ros-melodic-robotis-manipulator
 
+cd $HOME/open_manipulator_ws/
+catkin b
+
 echo "source $HOME/open_manipulator_ws/devel/setup.bash" >> ~/.bashrc
 sudo adduser $USER dialout
